4327c79db0a310f999be73d57ae6448789171b3c
Daniel Dóniz García <33846493+MrDoniz@users.noreply.github.com>
2022-10-10 22:20:06 +0100

    Add files via upload

---
+++ Práctica 1/nombreObjetos.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nombreObjetos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("El tag del objeto es: " + gameObject.tag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
